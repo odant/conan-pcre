1e3700ff564a537027c86ae29128f4e62b4882bf
d0nc1h0t <udincev@yandex.ru>
2019-11-15 11:10:57 +0300

    Add support armv7 architecture

--- conanfile.py
+++ conanfile.py
@@ -11,7 +11,7 @@ class PcreConan(ConanFile):
         "os": ["Windows", "Linux"],
         "compiler": ["Visual Studio", "gcc"],
         "build_type": ["Debug", "Release"],
-        "arch": ["x86_64", "x86", "mips"]
+        "arch": ["x86_64", "x86", "mips", "armv7"]
     }
     generators = "cmake"
     exports_sources = "pcre-8.41/*", "CMakeLists.txt", "FindPCRE.cmake"
